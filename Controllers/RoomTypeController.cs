48d012efd15ab8a59816af85720c122dd7e18c76
Vishal Singal <vishalsingal6@gmal.com>
2023-01-09 20:23:18 +0530

    GenericRepository Pattern Demostration With Rooms Model

---
+++ Controllers/RoomTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserRegExample.GenericRepo;
+using UserRegExample.Models;
+
+namespace UserRegExample.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        protected readonly IRepo<RoomType> _Repo;
+
+        public RoomTypeController(IRepo<RoomType> repo)
+        {
+            _Repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            var types = _Repo.GetAll();
+            return View(types);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(RoomType model)
+        {
+            _Repo.Create(model);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegExample.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int MaxAdults { get; set; }
+        public int MaxChilds { get; set; }
+        public decimal RoomFare { get; set; }
+        public string Description { get; set; }
+        public int NoOfBeds { get; set; }
+        public string RoomPictureUri { get; set; }
+        public RoomType TypeOfRoom { get; set; }
+        public int RoomTypeId { get; set; }
+        public RoomFacility TypeOfFacility { get; set; }
+        public int RoomFacilityId { get; set; }
+    }
+}
