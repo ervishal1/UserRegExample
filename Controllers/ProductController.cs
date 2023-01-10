cca6282b5bb5bf63899084d6b6cb6136d5c2bde0
Vishal Singal <vishalsingal6@gmal.com>
2023-01-10 13:35:42 +0530

    Product Search Added.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserRegExample.Data;
 using UserRegExample.GenericRepo;
 using UserRegExample.Models;
 
@@ -11,6 +12,14 @@ namespace UserRegExample.Controllers
     public class ProductController : Controller
     {
         protected readonly IRepo<Product> _Repo;
+        protected readonly ApplicationDbContext _Context;
+
+        public ProductController(IRepo<Product> repo, ApplicationDbContext context)
+        {
+            _Repo = repo;
+            _Context = context;
+        }
+
         public IActionResult Index()
         {
             var Product = _Repo.GetAll();
@@ -28,6 +37,22 @@ namespace UserRegExample.Controllers
         {
             _Repo.Create(model);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Search(string searchProductName = null)
+        {
+            if(String.IsNullOrEmpty(searchProductName))
+            { 
+                return View();
+            }
+            var searchResult = _Context.Products.Where(x => x.Title.Contains(searchProductName)).ToList();
+            if(searchResult.Count == 0)
+            {
+                ViewBag.products = "Search Product Not Found";
+                return View();
+            }
+
+            return View(searchResult);
         }
     }
 }
